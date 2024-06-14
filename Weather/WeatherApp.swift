6e23d5dddc7afde5ac1393a7eb6516e278504410
Raushan <developerraushan@gmail.com>
2024-06-14 12:54:15 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Raushan Kashyap on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
